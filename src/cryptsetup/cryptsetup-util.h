1993c0e3f2a0f53d89d0b85d668ed5637721f37e
Jonathan G. Underwood <jonathan.underwood@gmail.com>
2020-12-22 20:04:52 +0000

    cryptsetup: add support for workqueue options
    This commit adds support for disabling the read and write
    workqueues with the new crypttab options no-read-workqueue
    and no-write-workqueue. These correspond to the cryptsetup
    options --perf-no_read_workqueue and --perf-no_write_workqueue
    respectively.
    
    (cherry picked from commit 227acf0009bde2cd7f8bc371615b05e84137847d)

--- src/cryptsetup/cryptsetup-util.h
+++ src/cryptsetup/cryptsetup-util.h
@@ -11,3 +11,14 @@ int load_key_file(
                 uint64_t key_file_offset,
                 void **ret_key,
                 size_t *ret_key_size);
+
+#if HAVE_LIBCRYPTSETUP
+#include <libcryptsetup.h>
+
+/* These next two are defined in libcryptsetup.h from cryptsetup version 2.3.4 forwards. */
+#ifndef CRYPT_ACTIVATE_NO_READ_WORKQUEUE
+#define CRYPT_ACTIVATE_NO_READ_WORKQUEUE (1 << 24)
+#endif
+#ifndef CRYPT_ACTIVATE_NO_WRITE_WORKQUEUE
+#define CRYPT_ACTIVATE_NO_WRITE_WORKQUEUE (1 << 25)
+#endif
